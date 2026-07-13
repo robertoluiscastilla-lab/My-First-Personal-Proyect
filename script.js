07aa7785c61b260a22708b3bf0b1d3fcd9fcfd5e
Roberto Castilla <roberto.luiscastilla@gmail.com>
2026-07-12 23:58:10 -0500

    beginning experiments for the new layout after de enter in the search bar

--- script.js
+++ script.js
@@ -6,10 +6,24 @@ const top_images = document.querySelector(".top-images");
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
 const img3 = document.querySelector(".img3");
+const header = document.querySelector(".header");
+const main = document.querySelector("main");
+const third_container = document.querySelector(".third-container");
 
 //Other declaration
 
 const base_url = "https://image.tmdb.org/t/p/w780";
+
+//Clearing function
+
+function clearingScreen() {
+  const containers = [header, main, third_container];
+
+  containers.forEach((container) => {
+    container.innerHTML = "";
+    container.style.display = "none";
+  });
+}
 
 //Fetching the API
 import { API_KEY } from "./config.js";
@@ -46,9 +60,44 @@ async function getTopMovies() {
 
 getTopMovies();
 
+async function findMovies(movieName) {
+  try {
+    const fetched = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(movieName)}`,
+      options,
+    );
+
+    if (!fetched.ok) {
+      throw new Error(`Error in the petition: ${fetched.status}`);
+    }
+
+    const allMovieData = await fetched.json();
+
+    console.log(allMovieData);
+
+    //allMovieData.results.original_title
+
+    return allMovieData.results;
+  } catch (error) {
+    console.error("Problems during the launch fase: ", error.message);
+  }
+}
+
+findMovies("Obsession");
+
 //Event Listeners
 
 clearBtn.addEventListener("click", () => {
   searchBar.value = "";
   searchBar.focus();
+});
+
+searchBar.addEventListener("keydown", async (e) => {
+  if (e.key == "Enter") {
+    clearingScreen();
+
+    let typed = e.target.value;
+
+    const movies = await findMovies(typed);
+  }
 });
